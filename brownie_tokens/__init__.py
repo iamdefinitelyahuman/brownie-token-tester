3488fd523876c3cfbc936c5b55d46b05f2cf5fba
Roman <iromanagureev@gmail.com>
2021-06-21 18:00:13 +0300

    feat: add skip_holders and ETHPLORER_API_KEY

--- brownie_tokens/__init__.py
+++ brownie_tokens/__init__.py
@@ -1,6 +1,6 @@
 import brownie
 
-from brownie_tokens.forked import MintableForkToken
+from brownie_tokens.forked import BrownieTokensError, MintableForkToken, skip_holders
 from brownie_tokens.template import ERC20
 
 brownie.config["autofetch_sources"] = True
